f566b283c26aeab170e016968c065814ca57c196
Wellhite <wellhite@gmail.com>
2022-08-09 14:39:36 +0200

    update du composant d'accueil HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,76 @@
 <template>
-  <div>
-    <v-container fluid class="d-flex header align-center">
-      <v-row>
-        <v-col cols="7" class="text-center justify-center">
-          <div>
-            <img src="../assets/Boogie Burger header.png" contain alt="" />
+  <div id="app">
+    <v-container fluid class="d-flex header justify-center align-center">
+      <v-row class="text-center align-center justify-center">
+        <v-col
+          cols="12"
+          xl="7"
+          lg="7"
+          md="5"
+          sm="6"
+          class="text-center justify-center"
+        >
+          <div class="justify-center">
+            <img
+              src="../assets/Boogie Burger header.png"
+              aspect-ratio="16/9"
+              class="header-img"
+              alt=""
+            />
           </div>
 
-          <v-btn class="ma-2" x-large outlined color="primary">
+          <v-btn v-bind="size" class="ma-2" x-large outlined color="primary">
             Voir nos menus
           </v-btn>
         </v-col>
-        <v-col cols="5">
+        <v-col cols="12" xl="5" lg="5" md="7" sm="7">
           <v-spacer></v-spacer>
         </v-col>
       </v-row>
     </v-container>
     <v-container fluid class="news py-13">
-      <v-row no-gutters class="justify-center text-center">
-        <v-col class="py-5" cols="4">
-          <img contain src="../assets/banniere-Frites-maison.jpg" alt=""
+      <v-row class="justify-center text-center">
+        <v-col class="d-flex child-flex" cols="12" xl="4" lg="4" md="3" sm="6">
+          <img
+            contain
+            aspect-ratio="2"
+            max-width="500"
+            min-width="200"
+            src="../assets/banniere-Frites-maison.jpg"
+            alt=""
         /></v-col>
-        <v-col class="py-5" cols="4">
-          <img contain src="../assets/NOUVEAU-veggie-burger.jpg" alt=""
+        <v-col class="d-flex child-flex" cols="12" xl="4" lg="4" md="3" sm="6">
+          <img
+            contain
+            aspect-ratio="1"
+            max-width="500"
+            src="../assets/NOUVEAU-veggie-burger.jpg"
+            alt=""
         /></v-col>
-        <v-col cols="12" class="justify-center" color="black">
-          <img src="../assets/Banniere-Ice-Cream.jpg" alt="" />
+        <v-col
+          class="d-flex child-flex"
+          cols="12"
+          xl="8"
+          lg="8"
+          md="10"
+          sm="12"
+          color="black"
+        >
+          <img
+            contain
+            max-width="500"
+            src="../assets/Banniere-Ice-Cream.jpg"
+            alt=""
+          />
         </v-col>
       </v-row>
       <v-row class="justify-center my-12">
         <v-hover v-slot="{ hover }"
           ><v-btn
-            class="select text-lg-h6 font-weight-bold"
+            v-bind="size"
+            v-bind:class="{ active: currentFilter === 'menu' }"
+            v-on:click="setFilter('menu')"
+            class="filter select text-lg-h6 font-weight-bold"
             :style="{ color: hover ? '#F57C00' : 'white' }"
             plain
             >Nos menus</v-btn
@@ -39,7 +78,10 @@
         >
         <v-hover v-slot="{ hover }"
           ><v-btn
-            class="select text-lg-h6 font-weight-bold"
+            v-bind="size"
+            v-bind:class="{ active: currentFilter === 'burger' }"
+            @click="setFilter('burger')"
+            class="filter select text-lg-h6 font-weight-bold"
             :style="{ color: hover ? '#F57C00' : 'white' }"
             plain
             >Burger</v-btn
@@ -47,7 +89,10 @@
         >
         <v-hover v-slot="{ hover }"
           ><v-btn
-            class="select text-lg-h6 font-weight-bold"
+            v-bind="size"
+            v-bind:class="{ active: currentFilter === 'glace' }"
+            @click="setFilter('glace')"
+            class="filter select text-lg-h6 font-weight-bold"
             :style="{ color: hover ? '#F57C00' : 'white' }"
             plain
             >Glace</v-btn
@@ -55,147 +100,303 @@
         >
         <v-hover v-slot="{ hover }"
           ><v-btn
-            class="select text-lg-h6 font-weight-bold"
+            v-bind="size"
+            v-bind:class="{ active: currentFilter === 'boisson' }"
+            @click="setFilter('boisson')"
+            class="filter select text-lg-h6 font-weight-bold"
             :style="{ color: hover ? '#F57C00' : 'white' }"
             plain
             >Boisson</v-btn
           ></v-hover
         >
       </v-row>
-      <v-row class="justify-center">
-        <v-col cols="12" xl="2" lg="2" md="2" sm="4" class="text-center">
-          <v-card>
-            <img src="../assets/logo.png" alt="" />
-            <v-card-title>Cheese burger</v-card-title>
-            <v-card-text>
-              <v-row align="center">
-                <v-col cols="12">
-                  Smashburger looks for unique flavors & ingredients to build
-                  our local burgers.</v-col
-                >
-                <v-row class="my-1 align-center justify-center">
-                  <v-col cols="7" class="justify-center align-center">
-                    <h1 class="primary--text">9,99€</h1>
-                  </v-col>
 
-                </v-row>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="12" xl="2" lg="2" md="2" sm="4" class="text-center">
-          <v-card>
-            <img src="../assets/logo.png" alt="" />
-            <v-card-title>Burger farmer</v-card-title>
-            <v-card-text>
-              <v-row align="center">
-                <v-col cols="12">
-                  Smashburger looks for unique flavors & ingredients to build
-                  our local burgers.</v-col
+      <v-container>
+        <v-row class="justify-center">
+          <v-col
+            cols="6"
+            xl="3"
+            lg="3"
+            md="4"
+            sm="6"
+            class="text-center d-flex child-flex"
+            v-for="project in projects"
+            v-bind:key="project.title"
+          >
+            <div
+              class="d-flex project justify-center"
+              v-if="
+                currentFilter === project.category || currentFilter === 'ALL'
+              "
+            >
+              <transition name="fade">
+                <v-card
+                  max-width="300"
+                  elevation="7"
+                  class="magic-card justify-center text-center"
                 >
-                <v-row class="my-1 align-center justify-center">
-                  <v-col cols="7" class="justify-center align-center">
-                    <h1 class="primary--text">10,99€</h1>
-                  </v-col>
+                  <v-img :aspect-ratio="1" :src="project.img" alt=""></v-img>
+                  <v-card-title class="d-block" v-if="project.name">{{
+                    project.name
+                  }}</v-card-title>
+                  <v-card-text>
+                    <v-row align="center">
+                      <v-col cols="12">
+                        <v-divider class="mx-4"></v-divider
+                      ></v-col>
 
-                </v-row>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="12" xl="2" lg="2" md="2" sm="4" class="text-center">
-          <v-card>
-            <img src="../assets/logo.png" alt="" />
-            <v-card-title>Bacon burger</v-card-title>
-            <v-card-text>
-              <v-row align="center">
-                <v-col cols="12">
-                  Smashburger looks for unique flavors & ingredients to build
-                  our local burgers.</v-col
-                >
-                <v-row class="my-1 align-center justify-center">
-                  <v-col cols="7" class="justify-center align-center">
-                    <h1 class="primary--text">11,99€</h1>
-                  </v-col>
-
-                </v-row>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <v-col cols="12" xl="2" lg="2" md="2" sm="4" class="text-center">
-          <v-card>
-            <img src="../assets/logo.png" alt="" />
-            <v-card-title>Mexican burger</v-card-title>
-            <v-card-text>
-              <v-row align="center">
-                <v-col cols="12">
-                  Smashburger looks for unique flavors & ingredients to build
-                  our local burgers.</v-col
-                >
-                <v-row class="my-1 align-center justify-center">
-                  <v-col cols="7" class="justify-center align-center">
-                    <h1 class="primary--text">12,99€</h1>
-                  </v-col>
-                  
-                </v-row>
-              </v-row>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
+                      <v-row class="my-1 align-center justify-center">
+                        <v-col cols="5">
+                          <v-btn
+                            v-bind="size"
+                            rounded
+                            color="primary"
+                            class=" white--text font-weight-bold text-lg-caption"
+                          >
+                            ajouter <v-icon dark right> mdi-cart </v-icon>
+                          </v-btn>
+                        </v-col>
+                        <v-col cols="6">
+                          <h1
+                            class="primary--text font-weight-normal text-lg-h5"
+                            v-if="project.prix"
+                          >
+                            {{ project.prix }}€
+                          </h1>
+                        </v-col>
+                      </v-row>
+                    </v-row>
+                  </v-card-text>
+                </v-card>
+              </transition>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-container>
-    <v-container class="py-10">
-        <!-- -----------------------------------------------
-            Start Form Banner1
-        ----------------------------------------------- -->
-        <v-row justify="center">
-          <v-col cols="12" class="d-flex justify-center align-center">
-            <div class="text-center py-9">
-              <h2 class="text-lg-h3 font-weight-bold">
-                Inscrivez-vous à notre newsletter!
-              </h2>
-              <p class="mt-9 pt-4 text-lg-h6">
-                Recevez les dernières nouveautés, ainsi que les promotions.
-              </p>
-              <div class="mt-9 pt-2">
-                <form>
-                  <div class="d-flex ">
-                    <v-text-field
-                      placeholder="Enter Email Address"
-                      outlined 
-                    ><template slot="append">
+    <v-container fluid>
+      <countDown :date="end" @onFinish="finish()" ></countDown>
+    </v-container>
+    <v-parallax dark height="300" src="../assets/newsletter.jpg">
+      <v-row justify="center">
+        <v-col
+          cols="12"
+          xl="6"
+          lg="6"
+          md="6"
+          class="d-flex justify-center align-center"
+        >
+          <div class="text-center py-9">
+            <h2 class="primary--text text-lg-h3 font-weight-bold">
+              Inscrivez-vous à notre newsletter!
+            </h2>
+            <p class="white--text mt-9 text-lg-h6">
+              Recevez les dernières nouveautés, ainsi que les promotions.
+            </p>
+            <div class="mt-9 pt-2">
+              <v-form ref="form">
+                <v-text-field
+                  outlined
+                  solo
+                  placeholder="Entrez votre adresse mail"
+                >
+                  <template v-slot:append>
                     <v-btn
-                    
-        
-                      color="primary"
-                      class="text-uppercase font-weight-medium"
-                      
+                      v-bind="size"
+                      depressed
+                      plain
+                      class="
+                        primary--text
+                        text-uppercase
+                        font-weight-bold
+                        text-lg-body-1
+                      "
                     >
                       Envoyer
                     </v-btn>
-                     </template></v-text-field>
-                    
-                  </div>
-                </form>
-              </div>
+                  </template>
+                </v-text-field>
+              </v-form>
             </div>
-          </v-col>
-          
-        </v-row>
-
-        <!-- -----------------------------------------------
-            End Form Banner1
-        ----------------------------------------------- -->
-      </v-container>
+          </div>
+        </v-col>
+      </v-row>
+    </v-parallax>
   </div>
 </template>
 
 <script>
+import countDown from "./countDown.vue";
 export default {
   name: "HelloWorld",
+  components: { countDown },
 
-  data: () => ({}),
+  data: () => ({
+    end: new Date("2022-09-02T16:37:00"),
+    currentFilter: "burger",
+    projects: [
+      {
+        category: "menu",
+        img: require("../assets/logo.png"),
+        name: "Menu Kids",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "4,99",
+      },
+      {
+        category: "menu",
+        img: require("../assets/logo.png"),
+        name: "Menu Giant",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "menu",
+        img: require("../assets/logo.png"),
+        name: "Menu Texan",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "menu",
+        img: require("../assets/logo.png"),
+        name: "Menu XXL",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/spicyBurger.jpg"),
+        name: "Farmer Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/farmerBurger.jpg"),
+        name: "Spicy Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/towerBurger.jpg"),
+        name: "Tower Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/cheeseBurger.jpg"),
+        name: "Cheese Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/baconBurger.jpg"),
+        name: "Bacon Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/spaceBurger.jpeg"),
+        name: "Space Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/xllBurger.jpg"),
+        name: "XXL Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "burger",
+        img: require("../assets/spaceBurger.jpg"),
+        name: "Chicken Burger",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+
+      {
+        category: "glace",
+        img: require("../assets/logo.png"),
+        name: "Crunchy cookie",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "glace",
+        img: require("../assets/logo.png"),
+        name: "Coupe America",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "glace",
+        img: require("../assets/logo.png"),
+        name: "Baby Love",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "glace",
+        img: require("../assets/logo.png"),
+        name: "Choco Pistache",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "9,99",
+      },
+      {
+        category: "boisson",
+        img: require("../assets/logo.png"),
+        name: "Soda cola",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "2,99",
+      },
+      {
+        category: "boisson",
+        img: require("../assets/logo.png"),
+        name: "Limonade",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "2,99",
+      },
+      {
+        category: "boisson",
+        img: require("../assets/logo.png"),
+        name: "Orangade",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "2,99",
+      },
+      {
+        category: "boisson",
+        img: require("../assets/logo.png"),
+        name: "Eau aromatisé",
+        text: "Smashburger looks for unique flavors & ingredients to build our local burgers.",
+        prix: "2,99",
+      },
+    ],
+  }),
+
+  methods: {
+    setFilter(filter) {
+      this.currentFilter = filter;
+    },
+
+    finish() {
+      console.log("finish");
+    },
+  },
+
+  computed: {
+    size() {
+      const size = { xs: "x-small", sm: "small", lg: "large", xl: "x-large" }[
+        this.$vuetify.breakpoint.name
+      ];
+      return size ? { [size]: true } : {};
+    },
+  },
 };
 </script>
 <style scoped>
@@ -203,8 +404,26 @@ export default {
   background-image: url("../assets/header\ boogie\ burger.jpg");
   background-size: cover;
   background-repeat: no-repeat;
-  background-position: bottom;
-  height: 90vh;
+  background-position: center bottom;
+  width: 100%;
+  height: 43vw;
+}
+
+.header-img {
+  min-width: 50%;
+  max-width: 100%;
+}
+
+.fade-enter-active {
+  transition: opacity 3s ease-in-out;
+}
+
+.fade-enter-to {
+  opacity: 1;
+}
+
+.fade-enter {
+  opacity: 0;
 }
 
 .news {
@@ -220,4 +439,23 @@ export default {
   color: "#F57C00";
 }
 
+/* selecteur */
+.project {
+  transition: all 0.35s ease-in-out;
+}
+
+.projects-enter {
+  transform: scale(0.5) translatey(-80px);
+  opacity: 0;
+}
+
+.projects-leave-to {
+  transform: translatey(30px);
+  opacity: 0;
+}
+
+.projects-leave-active {
+  position: absolute;
+  z-index: -1;
+}
 </style>
