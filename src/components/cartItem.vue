182dfba36e50fd1a6e4b7b5226975f581f908f15
Wellhite <wellhite@gmail.com>
2022-10-21 07:55:04 +0200

    pop up panier avec affichage

---
+++ src/components/cartItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="cart-item">
+        <img class="cart-item__image" :src="item.media.source" />
+        <div class="cart-item__details">
+            <h4 class="cart-item__details-name">{{ item.name }}</h4>
+            <div class="cart-item__details-qty">
+              <p>{{ item.quantity }}</p>
+            </div>
+            <p class="cart-item__details-price">{{ item.line_total.formatted_with_symbol }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name:'cartItem',
+
+  props: ['product'],
+}
+</script>
+
+<style>
+
+</style>
