953f725982292b69271edf3822f1e933808580fa
Verb <58372226+Verb178@users.noreply.github.com>
2023-06-02 11:09:50 +0200

    Update App.vue
    Ajout d'un mode jour/nuit avec soleil/lune
    Ajout d'un bouton revenir tout en haut quand on descends la page

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,7 @@
   <v-app>
     <v-app-bar app fixed flat dark elevation="4" class="black align-center">
       <v-toolbar-title class="mx-12">
-        <v-img height="auto" max-width="5vw" contain src="./assets/logo_boogie_burger.svg"
-          alt="logo Webdesign and Com"></v-img>
+        <v-img height="auto" max-width="5vw" contain src="./assets/logo_boogie_burger.svg" alt="logo Webdesign and Com"></v-img>
       </v-toolbar-title>
       <v-spacer></v-spacer>
 
@@ -18,7 +17,7 @@
         <v-btn flat class="black white--text" @click="$vuetify.goTo('#avis')">
           Avis
         </v-btn>
-        <v-dialog v-model="contact" width="500"> <!-- le bouton appelle la methode contact qui ouvre la fenetre modale -->
+        <v-dialog v-model="contact" width="500">
           <template v-slot:activator="{ on }">
             <v-btn flat class="black white--text" v-on="on">
               Contact
@@ -27,26 +26,38 @@
           <Contact />
         </v-dialog>
 
-        <v-btn flat class="black">
+        <v-btn flat class="black" @click="dialog = true">
           <v-row justify="center">
-            <v-btn rounded icon class="primary white--text" data-toggle="modal" data-target="#cartModal"
-              @click.stop="dialog = true">
+            <v-btn rounded icon class="primary white--text">
               <v-icon medium>mdi-cart</v-icon>
-              <span v-if="cart && cart.line_items.length > 0">{{ totalItems }}</span>
+              <span class="badge badge-pill badge-danger">{{ totalItems }}</span>
             </v-btn>
             <v-dialog v-model="dialog" width="500">
               <Panier />
             </v-dialog>
           </v-row>
         </v-btn>
-      </div>
 
+        <v-btn flat class="black" @click="dialog = true">
+          <v-row justify="center">
+            <v-btn rounded icon class="primary white--text">
+              <v-icon medium>mdi-account</v-icon>
+            </v-btn>
+            <v-dialog v-model="dialog" width="500">
+              <Login />
+            </v-dialog>
+          </v-row>
+        </v-btn>
+
+        <v-btn icon @click="toggleTheme()">
+          <v-icon>{{ $vuetify.theme.dark ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
 
-    <v-navigation-drawer class="dark grey" absolute fixed left v-model="drawer">
+    <v-navigation-drawer class="dark grey" absolute fixed left v-model="drawer" hide-overlay>
       <v-list dense>
         <v-list-item-group v-model="group">
-          <v-list-item> </v-list-item>
           <v-list-item>
             <v-list-item-title @click="$vuetify.goTo('#services')">
               Nos Menus
@@ -72,190 +83,73 @@
     </v-navigation-drawer>
 
     <v-main>
-      <vueAccueil />
+      <v-container>
+        <!-- Le reste de votre contenu -->
+      </v-container>
     </v-main>
-    <v-footer dark padless>
-      <v-card width="100%" flat tile class="black white--text text-center">
-        <v-row class="justify-center align-center py-md-15 py-sm-6">
-          <!-- -----------------------------------------------
-        First Column
-      ----------------------------------------------- -->
-          <v-col cols="12" xl="3" lg="3" md="4" sm="12" class="
-                                        text-xl-left text-lg-left text-md-left text-sm-center
-                                        px-xs-0
-                                        order-2 order-sm-2 order-xl-1 order-lg-1 order-md-1
-                                      ">
-            <v-card-text class="
-                                          text-caption
-                                          text-xl-subtitle-1
-                                          text-lg-subtitle-1
-                                          text-md-body-2
-                                        ">
-              <p class="mt-4 mb-0">
-                <v-icon color="primary"> mdi-map-marker</v-icon>17 rue Leon
-                Gambetta, Lille
-              </p>
-              <p class="my-4">
-                <v-icon color="primary"> mdi-phone</v-icon> 06 07 08 09 10
-              </p>
-              <p class="mb-0">
-                <v-icon color="primary"> mdi-at</v-icon>
 
-                <a class="dark-link white--text" href="/">
-                  info@webdesign-and-com.fr</a>
-              </p>
-            </v-card-text>
-          </v-col>
-          <!-- -----------------------------------------------
-        Second Column
-      ----------------------------------------------- -->
-          <v-col cols="10" xl="3" lg="3" md="5" sm="9" class="px-xs-0 order-sm-1 order-xl-2 order-lg-2 order-md-2">
-            <v-card-text class="
-                                          white--text
-                                          text-justify
-                                          text-caption
-                                          text-xl-subtitle-1
-                                          text-lg-subtitle-1
-                                          text-md-body-2
-                                        ">
-              Boogie Burger est un site fictif réalise par
-              <a href="">Web Design & Com</a> comme outil démonstration pour sa
-              clientèle, les informations indiquées sur le site sont fictives
-              également. <br />
-            </v-card-text>
-          </v-col>
-          <!-- -----------------------------------------------
-        Third Column
-      ----------------------------------------------- -->
-          <v-col cols="9" xl="3" lg="3" md="10" sm="12" class="justify-center align-center order-sm-3">
-            <ul class="
-                                          pl-0
-                                          d-flex
-                                          flex-xl-column flex-lg-column flex-md-column
-                                          d-sm-inline-flex
-                                          align-center
-                                          justify-space-around justify-sm-space-between
-                                          text-caption
-                                          text-xl-subtitle-1
-                                          text-lg-subtitle-1
-                                          text-md-body-2
-                                        ">
-              <li class="my-1 mx-sm-1">
-                <a class="white--text" href="">Nos menus</a>
-              </li>
-              <li><a class="white--text mx-sm-1" href="">Nos produits</a></li>
-              <li class="my-1 mx-sm-1">
-                <a class="white--text" href="">Avis</a>
-              </li>
-              <li><a class="white--text mx-sm-1" href="">Contact</a></li>
-            </ul>
-          </v-col>
-        </v-row>
-
-        <v-card-text class="primary--text font-weight-bold">
-          {{ new Date().getFullYear() }}
-          <strong class="white--text"> - made by Web Design & Com</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
+    <v-btn
+      v-if="fab"
+      class="elevation-12"
+      fab
+      dark
+      fixed
+      bottom
+      right
+      color="primary"
+      @click="scrollTop"
+    >
+      <v-icon>mdi-arrow-up</v-icon>
+    </v-btn>
   </v-app>
 </template>
 
 <script>
-import vueAccueil from "./components/vueAccueil.vue";
-import Panier from "./components/panierClient.vue";
-import Contact from "./components/contactPage.vue"; // importe le composant contactPage.vue
+import Contact from './components/Contact.vue';
+import Panier from './components/Panier.vue';
+import Login from './components/Login.vue';
+
 export default {
-  name: "App",
-
   components: {
-    vueAccueil,
+    Contact,
     Panier,
-    Contact, // ajoute le composant contactPage.vue
+    Login,
   },
-
-  data: () => ({
-    drawer: false,
-    dialog: false,
-    group: null,
-    tab: null,
-    products: [],
-    cart: null,
-    carts: [],
-    totalItems: 0,
-  }),
-
-  watch: {
-    group() {
-      this.drawer = false;
+  data() {
+    return {
+      drawer: false,
+      contact: false,
+      dialog: false,
+      group: null,
+      fab: false,
+    };
+  },
+  computed: {
+    totalItems() {
+      // Calcul du nombre total d'éléments dans le panier
+      // Remplacez cette ligne par votre propre logique
+      return 0;
     },
   },
-
+  mounted() {
+    window.addEventListener('scroll', this.onScroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.onScroll);
+  },
   methods: {
-    created: function () {
-      setInterval(() => {
-        this.show = !this.show;
-      }, 2000);
-
-      this.fetchProducts();
-      this.fetchCart();
+    onScroll() {
+      this.fab = window.scrollY > 200;
     },
-
-    ajouterAuPanier(product) {
-    this.carts.push(product);
-  },
-
-    // compter le nombre d'items dans le panier et remplacer le chiffre dans le bouton
-    updateTotalItems() {
-      if (this.cart && this.cart.line_items) {
-        this.totalItems = this.cart.line_items.reduce(
-          (total, item) => total + item.quantity,
-          0
-        );
-      } else {
-        this.totalItems = 0;
-      }
+    scrollTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
     },
-
-    scrollTop: function () {
-      this.intervalId = setInterval(() => {
-        if (window.pageYOffset === 0) {
-          clearInterval(this.intervalId);
-        }
-        window.scroll(0, window.pageYOffset - 50);
-      }, 20);
+    toggleTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
     },
-
-    onScroll(e) {
-      if (typeof window === "undefined") return;
-      const top = window.pageYOffset || e.target.scrollTop || 0;
-      this.fab = top > 20;
-    },
-
-    mounted: function () {
-      window.addEventListener("scroll", this.onScroll);
-    },
-    beforeDestroy: function () {
-      window.removeEventListener("scroll", this.onScroll);
-    },
-
   },
 };
 </script>
-<style scoped>
-li {
-  list-style: none;
-}
-
-a {
-  text-decoration: none;
-}
-
-.navbar {
-  color: aliceblue;
-}
-
-v-btn {
-  color: white;
-}
-</style>
