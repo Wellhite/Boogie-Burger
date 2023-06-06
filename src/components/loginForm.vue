da9b514a0b19817def7a33a60aadeb9f1ea3c47d
Verb <58372226+Verb178@users.noreply.github.com>
2023-06-06 14:30:58 +0200

    Modification

---
+++ src/components/loginForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+      <!-- Login form -->
+
+        <v-list subheader two-line>
+            <v-subheader class="text-h6 black primary--text mb-4">Se connecter</v-subheader>
+            <v-list-item>
+            <v-list-item-content>
+                <v-list-item-title>
+                <v-text-field label="Email" v-model="email"></v-text-field>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                <v-text-field label="Mot de passe" v-model="password"></v-text-field>
+                </v-list-item-subtitle>
+            </v-list-item-content>
+            <v-list-item-action>
+                <v-btn color="primary" @click="submitForm">Connexion</v-btn>
+            </v-list-item-action>
+            </v-list-item>
+        </v-list>
+    </v-container>
+</template>
+
+<script>
+
+
+</script>
